bfe0941764e49dee228e9f46a5205c0d90f44236
AlexL70 <alexander.levinson.70@gmail.com>
2020-01-31 00:23:25 +0200

    Exercise 2 - done

--- Exercises/Exercise 2/app.js
+++ Exercises/Exercise 2/app.js
@@ -2,5 +2,14 @@ new Vue({
         el: '#exercise',
         data: {
             value: ''
-        }
+        },
+        methods: {
+            onClickAlertButton: function() {
+                alert('Alert!');
+            },
+            onInputKeydown: function(event) {
+                //console.log(event.target);
+                this.value = event.target.value;
+            }
+        },
     });
