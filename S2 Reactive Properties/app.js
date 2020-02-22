c62e11709cd297778469d82a08c00374434beaab
AlexL70 <alexander.levinson.70@gmail.com>
2020-02-22 13:21:05 +0200

    25. Reacting to Changes with Computed Properties

--- S2 Reactive Properties/app.js
+++ S2 Reactive Properties/app.js
@@ -1,6 +1,21 @@
 new Vue({
     el: '#app',
     data: {
-        name: 'Alex'
+        counter: 0,
+        secondCounter: 0
+    },
+    //  This section methods are called only if properties they depend on changes (if they are called in HTML code)
+    computed: {
+        output: function() {
+            console.log('Computed output called!');
+            return this.counter > 5 ? 'Greater than 5' : 'Smaller or equal to 5';
+        }
+    },
+    //  This section methods are called whenever something changes on page if where they output something
+    methods: {
+        outputSecond: function() {
+            console.log('Method output called!');
+            return 'Constant.';
+        }
     }
 });
