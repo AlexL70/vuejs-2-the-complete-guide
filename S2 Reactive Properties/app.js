9e6dba2815fa9edc444a89cd40721605ccea8628
AlexL70 <alexander.levinson.70@gmail.com>
2020-02-22 13:34:34 +0200

    26. An Alternative to Computed Properties: Watching for Changes

--- S2 Reactive Properties/app.js
+++ S2 Reactive Properties/app.js
@@ -2,20 +2,26 @@ new Vue({
     el: '#app',
     data: {
         counter: 0,
-        secondCounter: 0
+        secondCounter: 0,
+        outputSecond: ""
     },
-    //  This section methods are called only if properties they depend on changes (if they are called in HTML code)
+    //  This section methods are called only if properties they depend on changes
+    //  (if they are called in HTML code)
     computed: {
         output: function() {
             console.log('Computed output called!');
             return this.counter > 5 ? 'Greater than 5' : 'Smaller or equal to 5';
         }
     },
-    //  This section methods are called whenever something changes on page if where they output something
-    methods: {
-        outputSecond: function() {
-            console.log('Method output called!');
-            return 'Constant.';
+    //  This watches known properties from "data" section and executes code whenever appropriate property changes
+    //  It is less optimal way to do things than using computed properties
+    watch: {
+        secondCounter: function(value) {
+            vm = this;
+            //  Emuate calling server
+            setTimeout( function() {
+                vm.outputSecond = value > 5 ? "Second is greater than 5" : "Second is smaller or equal to 5";
+            }, 500)
         }
     }
 });
