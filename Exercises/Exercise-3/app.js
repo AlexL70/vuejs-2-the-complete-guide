2ab07cc29810a2fcdc04f3ea2adfc8e7e41b0af4
AlexL70 <alexander.levinson.70@gmail.com>
2020-03-31 14:06:49 +0300

    Exercise 3 - done

--- Exercises/Exercise-3/app.js
+++ Exercises/Exercise-3/app.js
@@ -2,5 +2,20 @@ new Vue({
         el: '#exercise',
         data: {
             value: 0
-        }
+        },
+        computed: {
+            result: function() {
+                return this.value >= 37 ? 'There!' : 'Not there yet';
+            }
+        },
+        watch: {
+            result: function() {
+                if(this.value != 0) {
+                    let self = this;
+                    setTimeout(function(){
+                            self.value = 0;
+                    }, 5000);
+                }
+            }
+        },
     });
